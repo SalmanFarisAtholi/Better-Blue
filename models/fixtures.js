a1f6fdb52fc75a41462f433bb681cfb9a135a52f
SalmanFarisAtholi <116656855+SalmanFarisAtholi@users.noreply.github.com>
2023-08-03 21:44:13 +0530

    fixtuer modal created

---
+++ models/fixtures.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const fixtureSchema = new mongoose.Schema({
+  opponent: {
+    type: String,
+    required: true,
+  },
+  logo: {
+    type: String,
+    required: true,
+  },
+  shortName: {
+    type: String,
+    required: true,
+  },
+  matchTime: {
+    type: Date,
+    required: true,
+  },
+  matchType: {
+    type: String,
+    required: true,
+  },
+  totalMatch: {
+    type: Number,
+    required: true,
+  },
+  win: {
+    type: Number,
+    required: true,
+  },
+  draw: {
+    type: Number,
+    required: true,
+  },
+  winProbability: {
+    type: Number,
+    required: true,
+  },
+  home: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("fixture", fixtureSchema);
